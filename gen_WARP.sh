ecb2f9c7755195d06f5f9e8b0978119a91ab7d68
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-14 16:28:15 +0500

    Update gen_WARP.sh
    убрал выбор DNS ))
    пока не нашёл 100% способ, как засунь его в Mihomo

--- gen_WARP.sh
+++ gen_WARP.sh
@@ -9,12 +9,6 @@ EP_LIST='Россия    |engage.cloudflareclient.com:4500
 Финляндия |fi.tribukvy.ltd:4501
 Финляндия |fi0.tribukvy.ltd:4501'
 
-DNS_LIST='Cloudflare|1.1.1.1, 2606:4700:4700::1111
-Google|8.8.8.8, 8.8.4.4, 2001:4860:4860::8888, 2001:4860:4860::8844
-malw.link|84.21.189.133, 64.188.98.242, 2a01:ecc0:2c1:2::2, 2a12:bec4:1460:d5::2
-GeoHide|45.155.204.190, 95.182.120.241, 2a0c:9300:0:54::1
-XBOX|87.228.47.200, 87.228.47.201, 2a00:ab00:533:6::200, 2a00:ab00:533:6::201'
-
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
@@ -23,32 +17,6 @@ MAGENTA="\033[1;35m"
 CYAN="\033[1;36m"
 
 clear
-
-chose_DNS() {
-DNS_SELECTED="1.1.1.1, 2606:4700:4700::1111"
-
-if [ "$ENDPOINT" = "engage.cloudflareclient.com:4500" ]; then
-
-echo -e "${MAGENTA}Выберите DNS:${NC}"
-
-i=1
-echo "$DNS_LIST" | while IFS='|' read -r name dns; do
-printf "${CYAN}%1d) ${GREEN}%s${NC}\n" "$i" "$name"
-i=$((i+1))
-done
-
-echo -en "\n${YELLOW}Введите номер (Enter = Cloudflare):${NC} "
-read dns_num
-
-MAX_NUM=$(echo "$DNS_LIST" | wc -l)
-
-if printf '%s' "$dns_num" | grep -qE '^[0-9]+$' && [ "$dns_num" -ge 1 ] && [ "$dns_num" -le "$MAX_NUM" ]; then
-DNS_SELECTED="$(echo "$DNS_LIST" | sed -n "${dns_num}p" | cut -d'|' -f2)"
-fi
-
-echo
-fi
-}
 
 chose_endpoint() {
 
@@ -199,7 +167,7 @@ exit 1
 fi
 
 ################################################################################################
-chose_endpoint; chose_DNS
+chose_endpoint
 ################################################################################################
 
 echo -e "${GREEN}Активируем и генерируем ${NC}WARP${NC}"
@@ -219,7 +187,7 @@ conf=$(cat <<EOF
 [Interface]
 PrivateKey = ${priv}
 Address = ${client_ipv4}, ${client_ipv6}
-DNS = ${DNS_SELECTED}
+DNS = 1.1.1.1, 2606:4700:4700::1111
 MTU = 1280
 S1 = 0
 S2 = 0
